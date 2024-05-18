2ad973c42e69e56fc7cee14f3f7829f00bfcee2b
shivam <shivammadlani5@gmail.com>
2024-05-18 13:28:46 +0530

    fixed deps issue made echoes more redable

--- setup.sh
+++ setup.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 #EXECUTE THIS SCRIPT IN HOME/{USER} DIRECCTORY#
 
-#install mcelog
-echo "===> fetching from repo..."
-git clone git://git.kernel.org/pub/scm/utils/cpu/mce/mcelog.git
-if [ $? -ne 0 ]
-then
-	echo "fetch failed :( please re-run"
-	exit 1
-	else
-	echo "===> fetched successfully"
-fi
-
-#install make
-echo "===> installing make..."
-sudo apt-get install make
+#installing necessary tools
+echo "===> installing build-essential and other necessary tools..."
+sudo apt-get install build-essential vim git cscope libncurses-dev libssl-dev bison flex git-email
 if [ $? -ne 0 ]
 then
 	echo "===> failed :( please re-run"
 	exit 1
 	else
-	echo "===> installed make successfully"
+	echo "===> installed successfully"
 fi
+
+#install mcelog
+echo "===> fetching from repo..."
+git clone git://git.kernel.org/pub/scm/utils/cpu/mce/mcelog.git
+if [ $? -ne 0 ]
+then
+	echo "===> fetch failed :( please re-run"
+	exit 1
+	else
+	echo "===> fetched successfully"
+fi
+
 
 echo "===> building and installing mcelog"
 cd mcelog
@@ -30,14 +31,14 @@ sudo make install
 
 if [ $? -ne 0 ]
 then
-	echo "some error occured"
+	echo "===> some error occured"
 	exit 1
 else
 	#copy to systemd
 	cp mcelog.service /usr/lib/systemd/system
 	if [ $? -e 0 ]
 	then
-		echo "error copying"
+		echo "===> error copying"
 		exit 1
 	fi
 	
@@ -45,19 +46,17 @@ else
 	sudo systemctl enable mcelog.service
 	if [ $? -e 0 ]
 	then
-		echo "successfuly started mcelog service"
-		echo "to verify run `mcelog --client` or `systemctl status mcelog.service`"
-		echo "removing mcelog directory..."
+		echo "===> successfuly started mcelog service"
+		echo "===> to verify run `mcelog --client` or `systemctl status mcelog.service`"
+		echo "===> removing mcelog directory..."
 		cd ../
 		rm -rf mcelog
 	else
-		echo "error occured in enabling service"
+		echo "===> error occured in enabling service"
 		exit 1
 	fi
 fi
 
-echo "installing required packages from apt"
-
-sudo apt-get install build-essential vim git cscope libncurses-dev libssl-dev bison flex git-email
+echo "===> installing required packages from apt"
 sudo apt-get install gcc make bash binutils flex bison pahole util-linux kmod e2fsprogs jfsutils reiserfsprogs xfsprogs squashfs-tools btrfs-progs pcmciautils quota pptpd nfs-common procps grub iptables openssl libcrypto++8 libcrypto++-dev bc cpio tar
 
