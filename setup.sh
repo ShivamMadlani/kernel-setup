95125dfd9bdff1e2917d56a62a498a6e182a86d0
shivam <shivammadlani5@gmail.com>
2024-05-18 14:08:05 +0530

    improved redability of logs

--- setup.sh
+++ setup.sh
@@ -1,62 +1,68 @@
 #!/bin/bash
 #EXECUTE THIS SCRIPT IN HOME/{USER} DIRECCTORY#
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
 
 #installing necessary tools
-echo "===> installing build-essential and other necessary tools..."
+echo -e "${YELLOW}===> installing build-essential and other necessary tools..."
 sudo apt-get install build-essential vim git cscope libncurses-dev libssl-dev bison flex git-email
 if [ $? -ne 0 ]
 then
-	echo "===> failed :( please re-run"
+	echo -e "${RED}===> failed :( please re-run"
 	exit 1
 	else
-	echo "===> installed successfully"
+	echo -e "${GREEN}===> installed successfully"
 fi
 
 #install mcelog
-echo "===> fetching from repo..."
+echo -e "${YELLOW}===> attempting mcelog installation..."
+echo -e "${YELLOW}===> fetching mcelog from repo..."
 git clone git://git.kernel.org/pub/scm/utils/cpu/mce/mcelog.git
 if [ $? -ne 0 ]
 then
-	echo "===> fetch failed :( please re-run"
+	echo -e "${RED}===> fetch failed :( please re-run"
 	exit 1
 	else
-	echo "===> fetched successfully"
+	echo -e "${GREEN}===> fetched successfully"
 fi
 
 
-echo "===> building and installing mcelog"
+echo -e "${YELLOW}===> building and installing mcelog"
 cd mcelog
 make
 sudo make install
 
 if [ $? -ne 0 ]
 then
-	echo "===> some error occured"
+	echo -e "${RED}===> some error occured"
 	exit 1
 else
 	#copy to systemd
-	cp mcelog.service /usr/lib/systemd/system
-	if [ $? -e 0 ]
+	cp mcelog/mcelog.service /usr/lib/systemd/system
+	if [ $? -eq 0 ]
 	then
-		echo "===> error copying"
+		echo -e "${RED}===> error copying"
 		exit 1
+	else
+		echo -e "${GREEN}===> copied mcelog.service to /usr/lib/systemd/system"
 	fi
 	
 	#enable service
 	sudo systemctl enable mcelog.service
-	if [ $? -e 0 ]
+	if [ $? -eq 0 ]
 	then
-		echo "===> successfuly started mcelog service"
-		echo "===> to verify run `mcelog --client` or `systemctl status mcelog.service`"
-		echo "===> removing mcelog directory..."
+		echo -e "${GREEN}===> successfuly started mcelog service"
+		echo -e "${GREEN}===> to verify run 'mcelog --client' or 'systemctl status mcelog.service'"
+		echo -e "${YELLOW}===> removing mcelog directory..."
 		cd ../
 		rm -rf mcelog
 	else
-		echo "===> error occured in enabling service"
+		echo -e "${RED}===> error occured in enabling service"
 		exit 1
 	fi
 fi
 
-echo "===> installing required packages from apt"
+echo -e "${YELLOW}===> installing required packages from apt"
 sudo apt-get install gcc make bash binutils flex bison pahole util-linux kmod e2fsprogs jfsutils reiserfsprogs xfsprogs squashfs-tools btrfs-progs pcmciautils quota pptpd nfs-common procps grub iptables openssl libcrypto++8 libcrypto++-dev bc cpio tar
-
+echo -e "%${GREEN}===> installation successful .bye. "
